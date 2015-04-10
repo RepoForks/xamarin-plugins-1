1693d71af16cd9a37dc21ccd236091b71e4a3ae1
Antonio Feregrino <antonio.feregrino@gmail.com>
2015-04-10 00:07:45 -0500

    Throws exception everywhere!

---
+++ M16.StoreInteraction/M16.StoreInteraction/M16.StoreInteraction.Forms.Plugin.Abstractions/IM16.StoreInteraction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace M16.StoreInteraction.Forms.Plugin.Abstractions
+{
+    /// <summary>
+    /// M16.StoreInteraction Interface
+    /// </summary>
+    public interface IStoreInteraction
+    {
+        /// <summary>
+        /// Opens the device's default app store for the given <paramref name="publisherName"/>
+        /// <remarks>Currently not working on iOS</remarks>
+        /// </summary>
+        /// <param name="publisherName">The publisher name (do not encode this string)</param>
+        void OpenForPublisher(string publisherName);
+
+        /// <summary>
+        /// Opens the device's default app store for the given <paramref name="appId"/>
+        /// <remarks>Currently not working on iOS</remarks>
+        /// </summary>
+        /// <param name="appId">The application id (do not encode this string)</param>
+        void OpenForApp(string appId);
+
+        /// <summary>
+        /// Opens the device's default app store for the given <paramref name="query"/>
+        /// <remarks>Currently not working on iOS</remarks>
+        /// </summary>
+        /// <param name="query">The keywords to search in the app store (do not encode this string)</param>
+        void OpenForQuery(string query);
+    }
+}
---
+++ M16.StoreInteraction/M16.StoreInteraction/M16.StoreInteraction.Forms.Plugin.iOSUnified/StoreInteractionImplementation.cs
@@ -0,0 +1,34 @@
+﻿using M16.StoreInteraction.Forms.Plugin.Abstractions;
+using System;
+using Xamarin.Forms;
+using M16.StoreInteraction.Forms.Plugin.iOSUnified;
+
+[assembly: Dependency(typeof(StoreInteractionImplementation))]
+namespace M16.StoreInteraction.Forms.Plugin.iOSUnified
+{
+    /// <summary>
+    /// M16.StoreInteraction Implementation
+    /// </summary>
+    public class StoreInteractionImplementation : IStoreInteraction
+    {
+        /// <summary>
+        /// Used for registration with dependency service
+        /// </summary>
+        public static void Init() { }
+
+        public void OpenForPublisher(string publisherName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OpenForApp(string appId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OpenForQuery(string query)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
