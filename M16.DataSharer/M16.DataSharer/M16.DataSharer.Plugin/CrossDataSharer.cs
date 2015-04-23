f5ae39b17a0af08f2d0aca76f1cab46b1d575dbc
Antonio Feregrino <antonio.feregrino@gmail.com>
2015-04-23 10:07:13 -0500

    New NuGet packages

--- M16.DataSharer/M16.DataSharer/M16.DataSharer.Plugin/CrossDataSharer.cs
+++ M16.DataSharer/M16.DataSharer/M16.DataSharer.Plugin/CrossDataSharer.cs
@@ -3,41 +3,41 @@ using System;
 
 namespace M16.DataSharer.Plugin
 {
-  /// <summary>
-  /// Cross platform M16.DataSharer implemenations
-  /// </summary>
-  public class CrossDataSharer
-  {
-    static Lazy<IDataSharer> Implementation = new Lazy<IDataSharer>(() => CreateDataSharer(), System.Threading.LazyThreadSafetyMode.PublicationOnly);
-
     /// <summary>
-    /// Current settings to use
+    /// Cross platform M16.DataSharer implemenations
     /// </summary>
-    public static IDataSharer Current
+    public class CrossDataSharer
     {
-      get
-      {
-        var ret = Implementation.Value;
-        if (ret == null)
-        {
-          throw NotImplementedInReferenceAssembly();
-        }
-        return ret;
-      }
-    }
+        static Lazy<IDataSharer> Implementation = new Lazy<IDataSharer>(() => CreateDataSharer(), System.Threading.LazyThreadSafetyMode.PublicationOnly);
 
-    static IDataSharer CreateDataSharer()
-    {
+        /// <summary>
+        /// Current settings to use
+        /// </summary>
+        public static IDataSharer Current
+        {
+            get
+            {
+                var ret = Implementation.Value;
+                if (ret == null)
+                {
+                    throw NotImplementedInReferenceAssembly();
+                }
+                return ret;
+            }
+        }
+
+        static IDataSharer CreateDataSharer()
+        {
 #if PORTABLE
-        return null;
+            return null;
 #else
         return new DataSharerImplementation();
 #endif
-    }
+        }
 
-    internal static Exception NotImplementedInReferenceAssembly()
-    {
-      return new NotImplementedException("This functionality is not implemented in the portable version of this assembly.  You should reference the NuGet package from your main application project in order to reference the platform-specific implementation.");
+        internal static Exception NotImplementedInReferenceAssembly()
+        {
+            return new NotImplementedException("This functionality is not implemented in the portable version of this assembly.  You should reference the NuGet package from your main application project in order to reference the platform-specific implementation.");
+        }
     }
-  }
 }
